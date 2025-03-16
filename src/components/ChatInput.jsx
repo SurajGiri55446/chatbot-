e20a74e6944e2b1e4bb46c29582e4b0091bfcd50
SurajGiri55446 <surajgiri@55446gmail.com>
2025-03-16 22:38:10 +0530

    Updated project with latest changes

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FiSend } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 const ChatInput = ({ onSubmit, isDarkMode }) => {
   const [question, setQuestion] = useState("");
@@ -13,29 +14,34 @@ const ChatInput = ({ onSubmit, isDarkMode }) => {
   };
 
   return (
-    <div className="flex items-center w-full">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+      className="flex items-center w-full"
+    >
       <input
         type="text"
         id="question"
-        placeholder="Ask  something..."
+        placeholder="Ask something..."
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
-        className={`flex-grow p-3 outline-none rounded-l-full ${
+        className={`flex-grow p-4 outline-none rounded-l-full transition-colors duration-300 ${
           isDarkMode
-            ? "bg-gray-700 text-gray-300 placeholder-gray-400"
+            ? "bg-gray-700 text-gray-100 placeholder-gray-400"
             : "bg-white text-gray-800 placeholder-gray-500"
         }`}
       />
       <button
         type="submit"
         onClick={handleSubmit}
-        className={`w-12 h-12 p-3 rounded-r-full ${
+        className={`w-14 h-14 p-4 rounded-r-full transition-colors duration-300 ${
           isDarkMode ? "bg-sky-500" : "bg-sky-600"
-        } text-white hover:bg-blue-700 transition duration-200 flex items-center justify-center`}
+        } text-white hover:bg-blue-700 flex items-center justify-center`}
       >
         <FiSend className="text-xl" />
       </button>
-    </div>
+    </motion.div>
   );
 };
 
